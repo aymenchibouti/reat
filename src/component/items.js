02c4167bbceeb42633da590e2c6fd23176b5dcc9
= <=>
2020-05-28 15:37:20 +0200

    css

--- src/component/items.js
+++ src/component/items.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import "./items.css";
 
 class items extends Component {
 
     render() {
         const { items } = this.props;
         const thItems = items.map(item => {
-            return item.age > 26 ? (
-                <div key={item.id}>
+            return item.age > 20 ? (
+                <div key={item.id} className="item">
                     <p>{item.name}</p>
                     <p>{item.age}</p>
                 </div>
