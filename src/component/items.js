268c345b246ad3408abc5cc80bc8ae4c3c5f9294
= <=>
2020-05-30 14:17:01 +0200

    lifeCycle

--- src/component/items.js
+++ src/component/items.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
 import "./items.css";
-
 class items extends Component {
+    // constructor
+    constructor(props) {
+        super(props);
+        console.log('constructor');
+    }
+    // componentDidMount
+    componentDidMount() {
+        console.log('componentDidMount');
+
+    }
+    // componentDidUpdate
+    componentDidUpdate(prevProps, prevState) {
+        console.log('componentDidUpdate', prevProps, prevState);
+    }
+
+
+    // render
 
     render() {
+        console.log('render');
         const { items } = this.props;
         const thItems = items.map(item => {
             return item.age > 20 ? (
@@ -16,6 +33,9 @@ class items extends Component {
         return (
             <div>
                 {thItems}
+                <button onClick={this.ok}>ok</button>
+                <button onClick={this.delete}>delete</button>
+
             </div >
         );
     }
